d97148e38397240b421f19aa2ab042886787b643
jatavena <ah0985@student.jamk.fi>
2025-08-25 22:13:41 +0300

    Fixes keypress bug
    Pressing return while typing to search bar does not return the user to login form.

--- src/presentation/SearchForm.jsx
+++ src/presentation/SearchForm.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
 const SearchForm = ( {handleSearch, handleSearchInput, searchInput} ) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        handleSearch();
+    };
+
     return (
         <>
         <form
+          onSubmit={handleSubmit}
           style={{'border': 'solid 1px', 'padding': 20, 'display': 'flex', 'justifyContent': 'space-evenly'}}
         >
           <input type="text" placeholder="type your search here" style={{'textAlign': 'center'}} value={searchInput} onChange={handleSearchInput} ></input>
           <br />
-          <button type="button" onClick={handleSearch} >Search</button>
+          <button type="submit" >Search</button>
         </form>
         </>
     );
